69cd20fae6c311df370aa62185a04f1a98abc27f
Oussama-Rahmouni <oussama.rahmouni.manager@gmail.com>
2024-02-26 16:12:27 +0100

    add user done

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import Sidebar from '../../component/sidebar/Sidebar'
-import Navbar from '../../component/navbar/Navbar'
+import "./list.scss"
+import Sidebar from "../../component/sidebar/Sidebar"
+import Navbar from "../../component/navbar/Navbar"
+import Datatable from "../../component/dataTable/DataTable"
 
 const List = () => {
   return (
-    <div className='list'>
-      <Sidebar />
+    <div className="list">
+      <Sidebar/>
       <div className="listContainer">
-        <Navbar />
-        data table
+        <Navbar/>
+        <Datatable/>
       </div>
     </div>
   )
 }
 
 export default List
-
